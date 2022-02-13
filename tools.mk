4c8f9d1197a92fcc83a64d55273031db456a9f09
Ryan Hartlage <ryanplusplus@gmail.com>
2022-02-12 20:58:54 -0500

    Simplified things a bit

--- tools.mk
+++ tools.mk
@@ -8,7 +8,7 @@ OBJCOPY := $(TOOLCHAIN_PREFIX)objcopy
 SIZE    := $(TOOLCHAIN_PREFIX)size
 
 define capture_version
-"$(shell $(1) --version | head -n 1)"
+"$(shell $1 --version | head -n 1)"
 endef
 
 CC_VERSION      := $(call capture_version,$(CC))
@@ -51,9 +51,9 @@ LDLIBS := \
 # $1 filename
 # $2 flags to capture
 define capture_flags
-$(shell mkdir -p $(dir $(1)))
-$(shell echo $(foreach flag,$(2),$(flag) $($(flag))) > $(1).next)
-$(shell if cmp -s $(1).next $(1); then rm $(1).next; else mv $(1).next $(1); fi)
+$(shell mkdir -p $(dir $1))
+$(shell echo $(foreach flag,$2,$(flag) $($(flag))) > $1.next)
+$(shell if cmp -s $1.next $1; then rm $1.next; else mv $1.next $1; fi)
 endef
 
 # $1 filename
@@ -63,26 +63,26 @@ endef
 # $5 CXXFLAGS
 # $6 build deps
 define generate_build_rule
-$$(BUILD_DIR)/$(1).o: $(1) $(6) $(lastword $(MAKEFILE_LIST))
-ifeq ($(suffix $(1)),.s)
+$$(BUILD_DIR)/$1.o: $1 $6 $(lastword $(MAKEFILE_LIST))
+ifeq ($(suffix $1),.s)
 	@echo Assembling $$(notdir $$@)...
 	@mkdir -p $$(dir $$@)
-	@$$(AS) $(2) $$< -o $$@
+	@$$(AS) $2 $$< -o $$@
 endif
-ifeq ($(suffix $(1)),.S)
+ifeq ($(suffix $1),.S)
 	@echo Assembling $$(notdir $$@)...
 	@mkdir -p $$(dir $$@)
-	@$$(CC) -c $(2) $$< $(3) -o $$@
+	@$$(CC) -c $2 $$< $3 -o $$@
 endif
-ifeq ($(suffix $(1)),.c)
+ifeq ($(suffix $1),.c)
 	@echo Compiling $$(notdir $$@)...
 	@mkdir -p $$(dir $$@)
-	@$$(CC) -x c -MMD -MP -MF "$$(@:%.o=%.d)" -MT "$$@" $(3) $(4) -c $$< -o $$@
+	@$$(CC) -x c -MMD -MP -MF "$$(@:%.o=%.d)" -MT "$$@" $3 $4 -c $$< -o $$@
 endif
-ifeq ($(suffix $(1)),.cpp)
+ifeq ($(suffix $1),.cpp)
 	@echo Compiling $$(notdir $$@)...
 	@mkdir -p $$(dir $$@)
-	@$$(CXX) -x c++ -MMD -MP -MF "$$(@:%.o=%.d)" -MT "$$@" $(3) $(5) -c $$< -o $$@
+	@$$(CXX) -x c++ -MMD -MP -MF "$$(@:%.o=%.d)" -MT "$$@" $3 $5 -c $$< -o $$@
 endif
 
 endef
@@ -90,45 +90,45 @@ endef
 # $1 lib name
 define generate_lib
 
-$(1)_INC_DIRS += $$($(1)_SRC_DIRS)
-$(1)_INC_FLAGS := $$(addprefix -iquote,$$($(1)_INC_DIRS))
+$1_INC_DIRS += $$($1_SRC_DIRS)
+$1_INC_FLAGS := $$(addprefix -iquote,$$($1_INC_DIRS))
 
-$(1)_SYS_INC_FLAGS := $$(addprefix -I,$$($(1)_SYS_INC_DIRS))
+$1_SYS_INC_FLAGS := $$(addprefix -I,$$($1_SYS_INC_DIRS))
 
-$(1)_CPPFLAGS := \
-  $$($(1)_INC_FLAGS) \
-  $$($(1)_SYS_INC_FLAGS) \
-  $$($(1)_CPPFLAGS) \
-  $$(addprefix -D,$$($(1)_DEFINES)) \
+$1_CPPFLAGS := \
+  $$($1_INC_FLAGS) \
+  $$($1_SYS_INC_FLAGS) \
+  $$($1_CPPFLAGS) \
+  $$(addprefix -D,$$($1_DEFINES)) \
 
-$(1)_LIB_SRCS := $$($(1)_SRC_FILES)
+$1_LIB_SRCS := $$($1_SRC_FILES)
 
-ifneq ($$($(1)_SRC_DIRS),)
-$(1)_LIB_SRCS += $$(shell find $$($(1)_SRC_DIRS) -maxdepth 1 -name *.cpp -or -name *.c -or -name *.s -or -name *.S)
+ifneq ($$($1_SRC_DIRS),)
+$1_LIB_SRCS += $$(shell find $$($1_SRC_DIRS) -maxdepth 1 -name *.cpp -or -name *.c -or -name *.s -or -name *.S)
 endif
 
-$(1)_LIB_SRCS := $$(sort $$($(1)_LIB_SRCS))
-$(1)_LIB_OBJS := $$($(1)_LIB_SRCS:%=$$(BUILD_DIR)/%.o)
-$(1)_LIB_DEPS := $$($(1)_LIB_SRCS:%=$$(BUILD_DIR)/%.d)
+$1_LIB_SRCS := $$(sort $$($1_LIB_SRCS))
+$1_LIB_OBJS := $$($1_LIB_SRCS:%=$$(BUILD_DIR)/%.o)
+$1_LIB_DEPS := $$($1_LIB_SRCS:%=$$(BUILD_DIR)/%.d)
 
-DEPS := $(DEPS) $$($(1)_LIB_DEPS)
+DEPS := $(DEPS) $$($1_LIB_DEPS)
 
-ifeq ($(2),LIB)
-unused := $$(call capture_flags,$$(BUILD_DIR)/lib_$(1).ar_flags,AR_VERSION)
+ifeq ($2,LIB)
+unused := $$(call capture_flags,$$(BUILD_DIR)/lib_$1.ar_flags,AR_VERSION)
 
-$$(BUILD_DIR)/$(1).lib: $$($1_LIB_OBJS) $$(BUILD_DIR)/lib_$(1).ar_flags
+$$(BUILD_DIR)/$1.lib: $$($1_LIB_OBJS) $$(BUILD_DIR)/lib_$1.ar_flags
 	@echo Building $$(notdir $$@)...
 	@mkdir -p $$(dir $$@)
 	@$$(AR) rcs $$@ $$^
 endif
 
-ifeq ($(2),INTERFACE_LIB)
-OBJS += $$($(1)_LIB_OBJS)
+ifeq ($2,INTERFACE_LIB)
+OBJS += $$($1_LIB_OBJS)
 endif
 
-unused := $$(call capture_flags,$$(BUILD_DIR)/lib_$(1).build_flags,AS_VERSION CC_VERSION CXX_VERSION AR_VERSION $(1)_ASFLAGS $(1)_CPPFLAGS $(1)_CFLAGS $(1)_CXXFLAGS)
+unused := $$(call capture_flags,$$(BUILD_DIR)/lib_$1.build_flags,AS_VERSION CC_VERSION CXX_VERSION AR_VERSION $1_ASFLAGS $1_CPPFLAGS $1_CFLAGS $1_CXXFLAGS)
 
-$$(foreach _src,$$($(1)_LIB_SRCS),$$(eval $$(call generate_build_rule,$$(_src),$$($(1)_ASFLAGS),$$($(1)_CPPFLAGS),$$($(1)_CFLAGS),$$($(1)_CXXFLAGS),$$(BUILD_DIR)/lib_$(1).build_flags)))
+$$(foreach _src,$$($1_LIB_SRCS),$$(eval $$(call generate_build_rule,$$(_src),$$($1_ASFLAGS),$$($1_CPPFLAGS),$$($1_CFLAGS),$$($1_CXXFLAGS),$$(BUILD_DIR)/lib_$1.build_flags)))
 
 endef
 
